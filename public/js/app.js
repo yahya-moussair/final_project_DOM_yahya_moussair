ead5f590463f1cb72638e13011b2d7b5cb9c04ce
yahya-mousair <yahyamoussair05@gmail.com>
2025-05-08 04:17:35 +0100

    complete hero section v: mobile and desktop , html css

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,30 @@
-let menuBtn = document.querySelector('#open')
-let closeBtn = document.querySelector('#close')
-let navBar = document.querySelector('.navLinks')
-let dropDown = document.querySelector('.dropDownDiv')
-let dropDownSelect = document.querySelector('.dropDownSelect')
-let openModal = document.querySelector('.openModal')
-let closeModal = document.querySelector('.closeModal')
-let modalDiv = document.querySelector('.modalDiv')
-menuBtn.addEventListener('click' , () => {
-    navBar.style.top = '50%'
-    body.style.background = '#ccc'
-    document.querySelector('button').disabled = true
-})
-closeBtn.addEventListener('click' , () => {
-    navBar.style.top = '-200%'
-    body.style.background = 'none'
-    document.querySelector('button').disabled = false
-})
+const headerJs = () => {
+  let menuBtn = document.querySelector("#open");
+  let closeBtn = document.querySelector("#close");
+  let navBar = document.querySelector(".navLinks");
+  let dropDown = document.querySelector(".dropDownDiv");
+  let dropDownSelect = document.querySelector(".dropDownSelect");
+  let openModal = document.querySelector(".openModal");
+  let closeModal = document.querySelector(".closeModal");
+  let modalDiv = document.querySelector(".modalDiv");
+  menuBtn.addEventListener("click", () => {
+    navBar.style.top = "50%";
+  });
+  closeBtn.addEventListener("click", () => {
+    navBar.style.top = "-200%";
+  });
 
-dropDown.addEventListener('mouseover' , () => {
-    dropDownSelect.classList.remove('hidden')
-})
-dropDownSelect.addEventListener('mouseout' , () => {
-    dropDownSelect.classList.add('hidden')
-})
-openModal.addEventListener('click' , () => {
-    modalDiv.style.top = '2vh'
-    document.querySelector('button').disabled = true
-})
-closeModal.addEventListener('click' , () => {
-    modalDiv.style.top = '-200%'
-    document.querySelector('button').disabled = false
-})
+  dropDown.addEventListener("mouseover", () => {
+    dropDownSelect.classList.remove("hidden");
+  });
+  dropDownSelect.addEventListener("mouseout", () => {
+    dropDownSelect.classList.add("hidden");
+  });
+  openModal.addEventListener("click", () => {
+    modalDiv.style.top = "2vh";
+  });
+  closeModal.addEventListener("click", () => {
+    modalDiv.style.top = "-200%";
+  });
+};
+headerJs();
